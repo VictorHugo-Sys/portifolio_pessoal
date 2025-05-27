e1ffec11914455991df770cb88b6f32ae1f5eaba
ykatanay <vicdfarias@gmail.com>
2025-05-27 10:11:26 -0300

    step 2

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,16 @@ import { motion, useAnimation } from 'framer-motion';
 import { useInView } from 'react-intersection-observer';
 import { TypeAnimation } from 'react-type-animation';
 import perfil from '../imgs/perfilofc.png';
+import react from '../imgs/svgs/react.svg';
+import js from '../imgs/svgs/js.svg';
+import css from '../imgs/svgs/css.svg';
+import router from '../imgs/svgs/router.svg';
+import tailwind from '../imgs/svgs/tailwind.svg';
+import word from '../imgs/svgs/word.svg';
+import sass from '../imgs/svgs/sass.svg';
+import typescript from '../imgs/svgs/typescript.svg';
+import figma from '../imgs/svgs/figma.svg';
+import python from '../imgs/svgs/python.svg';
 
 const App = () => {
   const controls = useAnimation();
@@ -15,7 +25,7 @@ const App = () => {
   }, [controls, inView]);
 
   const controls1 = useAnimation();
-  const [ref1, inView1] = useInView({ triggerOnce: true, threshold: 0.8 });
+  const [ref1, inView1] = useInView({ triggerOnce: true, threshold: 0.5 });
 
   React.useEffect(() => {
     if (inView1) {
@@ -31,7 +41,6 @@ const App = () => {
       controls2.start('visible');
     }
   }, [controls2, inView2]);
-  
 
   return (
     <main>
@@ -117,66 +126,55 @@ const App = () => {
         }}
         className=""
       >
-        <div className='grid grid-cols-3 max-w-5xl my-40 mx-auto px-5 gap-8'>
+        <div className="grid grid-cols-3 max-w-5xl my-40 mx-auto px-5 gap-8">
           <h1
-                className="relative col-span-3 font-bold text-5xl bg-gradient-to-r from-fuchsia-500 to-cyan-500 bg-clip-text text-transparent capitalize
+            className="relative col-span-3 font-bold text-5xl bg-gradient-to-r from-fuchsia-500 to-cyan-500 bg-clip-text text-transparent capitalize
                 before:absolute before:bg-fuchsia-500 before:w-14 before:h-1 before:-left-6 before:top-1 before:-rotate-45 inline-block"
-              >
-                conhecimento | <span className='bg-gradient-to-r from-amber-200 to-yellow-500 bg-clip-text text-transparent inline-block'>estudando</span>
-              </h1>
-          <div className='flex gap-4 border-4 border-violet-500 bg-violet-950 p-5 rounded-md text-2xl text-white hover:scale-105 items-center transition text-center'>
-              <p>
-                react
-              </p>
-              <img src="" alt="" />
+          >
+            conhecimento |{' '}
+            <span className="bg-gradient-to-r from-amber-200 to-yellow-500 bg-clip-text text-transparent inline-block">
+              estudando
+            </span>
+          </h1>
+          <div className="flex gap-4 border-4 border-violet-500 bg-violet-950 p-5 rounded-md text-2xl text-white hover:scale-105 items-center transition text-center justify-between">
+            <p>react</p>
+            <img src={react} className="w-10 h-10 object-contain" />
           </div>
-          <div className='flex gap-4 border-4 border-violet-500 bg-violet-950 p-5 rounded-md text-2xl text-white hover:scale-105 items-center transition'>
-              <p>
-                js
-              </p>
-              <img src="" alt="" />
+          <div className="flex gap-4 border-4 border-violet-500 bg-violet-950 p-5 rounded-md text-2xl text-white hover:scale-105 items-center transition justify-between">
+            <p>js</p>
+            <img src={js} className="w-10 h-10 object-contain" />
           </div>
-          <div className='flex gap-4 border-4 border-violet-500 bg-violet-950 p-5 rounded-md text-2xl text-white hover:scale-105 items-center transition'>
-              <p>
-                css
-              </p>
-              <img src="" alt="" />
+          <div className="flex gap-4 border-4 border-violet-500 bg-violet-950 p-5 rounded-md text-2xl text-white hover:scale-105 items-center transition justify-between">
+            <p>css</p>
+            <img src={css} className="w-10 h-10 object-contain"/>
           </div>
-          <div className='flex gap-4 border-4 border-violet-500 bg-violet-950 p-5 rounded-md text-2xl text-white hover:scale-105 items-center transition'>
-              <p>
-                react router
-              </p>
-              <img src="" alt="" />
+          <div className="flex gap-4 border-4 border-violet-500 bg-violet-950 p-5 rounded-md text-2xl text-white hover:scale-105 items-center transition justify-between">
+            <p>router</p>
+            <img src={router} className="w-10 h-10 object-contain" />
           </div>
-          <div className='flex gap-4 border-4 border-violet-500 bg-violet-950 p-5 rounded-md text-2xl text-white hover:scale-105 items-center transition'>
-              <p>
-                react
-              </p>
-              <img src="" alt="" />
+          <div className="flex gap-4 border-4 border-violet-500 bg-violet-950 p-5 rounded-md text-2xl text-white hover:scale-105 items-center transition justify-between">
+            <p>tailwind</p>
+            <img src={tailwind} className="w-10 h-10 object-contain" />
           </div>
-          <div className='flex gap-4 border-4 border-amber-400 bg-violet-950 p-5 rounded-md text-2xl text-white hover:scale-105 items-center transition'>
-              <p>
-                REST API
-              </p>
-              <img src="" alt="" />
+          <div className="flex gap-4 border-4 border-amber-400 bg-violet-950 p-5 rounded-md text-2xl text-white hover:scale-105 items-center transition justify-between">
+            <p>REST API</p>
+            <img src={word} className="w-10 h-10 object-contain"/>
           </div>
-          <div className='flex gap-4 border-4 border-amber-400 bg-violet-950 p-5 rounded-md text-2xl text-white hover:scale-105 items-center transition'>
-              <p>
-                sass
-              </p>
-              <img src="" alt="" />
+          <div className="flex gap-4 border-4 border-amber-400 bg-violet-950 p-5 rounded-md text-2xl text-white hover:scale-105 items-center transition justify-between">
+            <p>sass</p>
+            <img src={sass} className="w-10 h-10 object-contain" />
           </div>
-          <div className='flex gap-4 border-4 border-amber-400 bg-violet-950 p-5 rounded-md text-2xl text-white hover:scale-105 items-center transition'>
-              <p>
-                typescript
-              </p>
-              <img src="" alt="" />
+          <div className="flex gap-4 border-4 border-amber-400 bg-violet-950 p-5 rounded-md text-2xl text-white hover:scale-105 items-center transition justify-between">
+            <p>typescript</p>
+            <img src={typescript} className="w-10 h-10 object-contain"/>
           </div>
-          <div className='flex gap-4 border-4 border-amber-400 bg-violet-950 p-5 rounded-md text-2xl text-white hover:scale-105 items-center transition'>
-              <p>
-                UI/UX
-              </p>
-              <img src="" alt="" />
+          <div className="flex gap-4 border-4 border-amber-400 bg-violet-950 p-5 rounded-md text-2xl text-white hover:scale-105 items-center transition justify-between">
+            <p>UI/UX</p>
+            <img src={figma} className="w-10 h-10 object-contain"/>
+          </div>
+          <div className="flex gap-4 border-4 border-amber-400 bg-violet-950 p-5 rounded-md text-2xl text-white hover:scale-105 items-center transition justify-between">
+            <p>python</p>
+            <img src={python} className="w-10 h-10 object-contain"/>
           </div>
         </div>
       </motion.div>
