4c4ecc1efa5e850b96ee2189fc8e2c306bb0a671
ykatanay <vicdfarias@gmail.com>
2025-05-28 17:53:05 -0300

    .

--- src/conhecimentos.jsx
+++ src/conhecimentos.jsx
@@ -13,57 +13,57 @@ import python from '../imgs/svgs/python.svg';
 
 const Conhecimentos = () => {
   return (
-    <div className="grid grid-cols-3 max-w-5xl mb-32 mx-auto px-8 gap-8 font-sans max-md:grid-cols-1">
+    <div className="grid grid-cols-3 max-w-5xl mb-32 mx-auto px-8 gap-8 font-sans max-md:flex max-md:flex-col max-md:items-center">
       <h1
         className="relative col-span-3 font-bold text-5xl bg-gradient-to-r from-fuchsia-500 to-cyan-500 bg-clip-text text-transparent capitalize
-                before:absolute before:bg-fuchsia-500 before:w-14 before:h-1 before:-left-6 before:top-1 before:-rotate-45 inline-block max-md:col-span-1 max-md:text-3xl"
+                before:absolute before:bg-fuchsia-500 before:w-14 before:h-1 before:-left-6 before:top-1 before:-rotate-45 inline-block  max-md:text-3xl"
       >
         conhecimento |{' '}
         <span className="bg-gradient-to-r from-amber-200 to-yellow-500 bg-clip-text text-transparent inline-block">
           estudando
         </span>
       </h1>
-      <div className="box-purple">
+      <div className="box-purple max-md:max-w-[70vw] max-md:scale-90">
         <p>react</p>
         <img src={react} className="w-10 h-10 object-contain" />
       </div>
-      <div className="box-purple">
+      <div className="box-purple max-md:max-w-[70vw] max-md:scale-90">
         <p>js</p>
         <img src={js} className="w-10 h-10 object-contain" />
       </div>
-      <div className="box-purple">
+      <div className="box-purple max-md:max-w-[70vw] max-md:scale-90">
         <p>css</p>
         <img src={css} className="w-10 h-10 object-contain" />
       </div>
-      <div className="box-purple">
+      <div className="box-purple max-md:max-w-[70vw] max-md:scale-90">
         <p>router</p>
         <img src={router} className="w-10 h-10 object-contain" />
       </div>
-      <div className="box-purple">
+      <div className="box-purple max-md:max-w-[70vw] max-md:scale-90">
         <p>tailwind</p>
         <img src={tailwind} className="w-10 h-10 object-contain" />
       </div>
-      <div className="box-purple">
+      <div className="box-purple max-md:max-w-[70vw] max-md:scale-90">
         <p>git</p>
         <img src={git} className="w-10 h-10 object-contain" />
       </div>
-      <div className="box-yellow">
+      <div className="box-yellow max-md:max-w-[70vw] max-md:scale-90">
         <p>Rest api</p>
-        <img src={word} className="w-10 h-10 object-contain" />
+        <img src={word} className="w-10 h-10 object-contain " />
       </div>
-      <div className="box-yellow">
+      <div className="box-yellow max-md:max-w-[70vw] max-md:scale-90">
         <p>sass</p>
         <img src={sass} className="w-10 h-10 object-contain" />
       </div>
-      <div className="box-yellow">
+      <div className="box-yellow max-md:max-w-[70vw] max-md:scale-90">
         <p>typescript</p>
         <img src={typescript} className="w-10 h-10 object-contain" />
       </div>
-      <div className="box-yellow">
+      <div className="box-yellow max-md:max-w-[70vw] max-md:scale-90">
         <p>UI/UX</p>
         <img src={figma} className="w-10 h-10 object-contain" />
       </div>
-      <div className="box-yellow">
+      <div className="box-yellow max-md:max-w-[70vw] max-md:scale-90">
         <p>python</p>
         <img src={python} className="w-10 h-10 object-contain" />
       </div>
